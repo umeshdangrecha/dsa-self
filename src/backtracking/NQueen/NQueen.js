fc26a4409738a4acf87f55c6f4165375b9a597dd
Umesh Dangrecha <umeshdangrecha240@gmail.com>
2024-02-11 16:04:15 +0530

    feat: 🎸 count solvs

--- src/backtracking/NQueen/NQueen.js
+++ src/backtracking/NQueen/NQueen.js
@@ -18,6 +18,12 @@ const isSafe = (board, row, col, N) => {
       return false
     }
   }
+  // check if there is a queen in the upper right diagonal
+  for (let i = row, j = col; i >= 0 && j < N; i--, j++) {
+    if (board[i][j] === 1) {
+      return false
+    }
+  }
   // check diagonals left bottom
   for (let i = row, j = col; i < N && j >= 0; i++, j--) {
     if (board[i][j] === 1) {
@@ -27,12 +33,12 @@ const isSafe = (board, row, col, N) => {
   return true
 }
 
-const util = (N, row, board) => {
+const util = (N, row, board, start = 0) => {
   if (row === N) {
     return true
   }
-
-  for (let col = 0; col < N; col++) {
+  const r = row === 0 ? start : 0
+  for (let col = r; col < N; col++) {
     if (isSafe(board, row, col, N)) {
       board[row][col] = 1
       if (util(N, row + 1, board)) {
@@ -48,4 +54,42 @@ export function nQueen(N) {
 
   util(N, 0, board)
   return board
+}
+
+const getStart = (board, N) => {
+  let start = 0
+
+  for (let i = 0; i < N; i++) {
+    if (board[0][i] === 1) {
+      start = i + 1
+    }
+  }
+  return start
+}
+
+const getAns = (board, N) => {
+  let start = []
+
+  for (let i = 0; i < N; i++) {
+    for (let j = 0; j < N; j++) {
+      if (board[i][j] === 1) {
+        start.push(j + 1)
+      }
+    }
+  }
+  return start
+}
+
+export function nQueenPossibleSols(N) {
+  let board = Array.from({ length: N }, () => Array(N).fill(0))
+  let s = []
+  let start = 0
+  for (let i = 0; i < N; i++) {
+    if (util(N, 0, board, start)) {
+      s.push(getAns(board, N))
+      start = getStart(board, N)
+      board = Array.from({ length: N }, () => Array(N).fill(0))
+    }
+  }
+  return s
 }
--- src/backtracking/NQueen/NQueen.test.js
+++ src/backtracking/NQueen/NQueen.test.js
@@ -1,4 +1,4 @@
-import { nQueen, getPossibleSolutionCount } from './NQueen'
+import { nQueen, nQueenPossibleSols } from './NQueen'
 
 test('4 - NQueen Problem', () => {
   const res = [
@@ -13,13 +13,44 @@ test('4 - NQueen Problem', () => {
 test('8 - NQueen Problem', () => {
   const res = [
     [1, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 1, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 1, 0, 0],
+    [0, 0, 0, 0, 1, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 1],
+    [0, 0, 0, 0, 0, 1, 0, 0],
+    [0, 0, 1, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 1, 0],
-    [0, 0, 0, 1, 0, 0, 0, 0],
     [0, 1, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 1, 0, 0, 0]
+    [0, 0, 0, 1, 0, 0, 0, 0]
   ]
   expect(nQueen(8)).toEqual(res)
+})
+
+test('8 - NQueen Problem possible solvs', () => {
+  // const res = [
+  //   [1, 3, 6, 8, 7, 4, 2, 5],
+  //   [2, 1, 6, 8, 7, 3, 5, 4],
+  //   [3, 1, 6, 8, 2, 7, 5, 4],
+  //   [4, 1, 5, 8, 2, 7, 3, 6],
+  //   [5, 1, 3, 6, 8, 7, 2, 4],
+  //   [6, 1, 3, 7, 2, 8, 5, 4],
+  //   [7, 1, 3, 5, 8, 2, 4, 6]
+  // ]
+  const res = [
+    [1, 5, 8, 6, 3, 7, 2, 4],
+    [2, 4, 6, 8, 3, 1, 7, 5],
+    [3, 1, 7, 5, 8, 2, 4, 6],
+    [4, 1, 5, 8, 2, 7, 3, 6],
+    [5, 1, 4, 6, 8, 2, 7, 3],
+    [6, 1, 5, 2, 8, 3, 7, 4],
+    [7, 1, 3, 8, 6, 4, 2, 5],
+    [8, 2, 4, 1, 7, 5, 3, 6]
+  ]
+  expect(nQueenPossibleSols(8)).toEqual(res)
+})
+
+test('4 - NQueen Problem possible solvs', () => {
+  const res = [
+    [2, 4, 1, 3],
+    [3, 1, 4, 2]
+  ]
+  expect(nQueenPossibleSols(4)).toEqual(res)
 })
